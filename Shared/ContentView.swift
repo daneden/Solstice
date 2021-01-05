de0f60f221934a7846cf2333d12164a401891192
Daniel Eden <dan.eden@me.com>
2021-01-05 18:38:52 +0000

    Initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import Combine
+import CoreLocation
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @ObservedObject var locationManager = LocationManager.shared
+  
+  var solarToday: Solar? {
+    guard let coord =
+            locationManager.location?.coordinate,
+            CLLocationCoordinate2DIsValid(coord) else { return nil }
+    return Solar(coordinate: coord)
+  }
+  
+  var solarYesterday: Solar? {
+    guard let coord =
+            locationManager.location?.coordinate,
+          CLLocationCoordinate2DIsValid(coord) else { return nil }
+    guard let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date()) else { return nil }
+    
+    return Solar(for: yesterday, coordinate: coord)
+  }
+  
+  var body: some View {
+    if let sunsetToday = solarToday?.sunset,
+       let sunsetYesterday = solarYesterday?.sunset {
+      Text(sunsetToday, style: .time)
+      Text(sunsetYesterday, style: .time)
+      Text(locationManager.location?.coordinate.latitude.description ?? "0")
+      Text(Date(), style: .time)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ Shared/LocationManager.swift
@@ -0,0 +1,72 @@
+//
+//  LocationManager.swift
+//  Solstice
+//
+//  Created by Daniel Eden on 05/01/2021.
+//
+
+import Foundation
+import Combine
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+  let objectWillChange = PassthroughSubject<Void, Never>()
+  static let shared = LocationManager()
+  
+  private let locationManager = CLLocationManager()
+  private let geocoder = CLGeocoder()
+  
+  @Published var status: CLAuthorizationStatus? {
+    willSet { objectWillChange.send() }
+  }
+  
+  @Published var location: CLLocation? {
+    willSet { objectWillChange.send() }
+  }
+  
+  @Published var placemark: CLPlacemark? {
+    willSet { objectWillChange.send() }
+  }
+  
+  override init() {
+    super.init()
+    
+    self.locationManager.delegate = self
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyReduced
+    self.locationManager.requestWhenInUseAuthorization()
+    self.locationManager.startUpdatingLocation()
+  }
+  
+  private func geocode() {
+    guard let location = self.location else { return }
+    geocoder.reverseGeocodeLocation(location, completionHandler: { (places, error) in
+      if error == nil {
+        self.placemark = places?[0]
+      } else {
+        self.placemark = nil
+      }
+    })
+  }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    self.status = status
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let location = locations.last else { return }
+    self.location = location
+    self.geocode()
+  }
+}
+
+extension CLLocation {
+  var latitude: Double {
+    return self.coordinate.latitude
+  }
+  
+  var longitude: Double {
+    return self.coordinate.longitude
+  }
+}
