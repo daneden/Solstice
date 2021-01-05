7203fd83b58ed86d620e948b37156836eb711422
Daniel Eden <dan.eden@me.com>
2021-01-05 17:18:20 +0000

    Initial Commit

---
+++ Shared/SolsticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolsticeApp.swift
+//  Shared
+//
+//  Created by Daniel Eden on 05/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SolsticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
