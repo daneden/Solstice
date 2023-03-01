215973f2fc32982e37e767a84ed62f49ddb97156
Daniel Eden <dan.eden@me.com>
2023-03-01 10:19:08 +0000

    Add shortcuts

---
+++ Intents/GetSunsetTime.swift
@@ -0,0 +1,36 @@
+//
+//  GetSunsetTime.swift
+//  Solstice
+//
+//  Created by Daniel Eden on 01/03/2023.
+//
+
+import Foundation
+import AppIntents
+import CoreLocation
+import Solar
+
+struct GetSunsetTime: AppIntent {
+	static var title: LocalizedStringResource = "Get Sunset Time"
+	static var description = IntentDescription("Calculate the sunset time on a given date in a given location")
+	
+	@Parameter(title: "Date")
+	var date: Date
+	
+	@Parameter(title: "Location")
+	var location: CLPlacemark
+	
+	static var parameterSummary: some ParameterSummary {
+		Summary("Get the sunset time on \(\.$date) in \(\.$location)")
+	}
+	
+	func perform() async throws -> some IntentResult {
+		guard let coordinate = location.location?.coordinate else {
+			throw $location.needsValueError("What location do you want to see the sunset for?")
+		}
+		
+		let solar = Solar(for: date, coordinate: coordinate)
+		
+		return .result(value: solar?.sunset)
+	}
+}
---
+++ Intents/ViewRemainingDaylight.swift
@@ -0,0 +1,42 @@
+//
+//  ViewRemainingDaylight.swift
+//  Solstice
+//
+//  Created by Daniel Eden on 01/03/2023.
+//
+
+import Foundation
+import AppIntents
+import Solar
+import CoreLocation
+
+struct ViewRemainingDaylight: AppIntent {
+	static var title: LocalizedStringResource = "View Remaining Daylight"
+	static var description = IntentDescription("View how much daylight is remaining today, based on the time until sunset.")
+	
+	@Parameter(title: "Location")
+	var location: CLPlacemark
+	
+	static var parameterSummary: some ParameterSummary {
+		Summary("Get today's remaining daylight in \(\.$location)")
+	}
+	
+	func perform() async throws -> some IntentResult {
+		guard let coordinate = location.location?.coordinate else {
+			throw $location.needsValueError("What location do you want to see the daylight for?")
+		}
+		
+		let solar = Solar(coordinate: coordinate)!
+		let isDaytime = solar.safeSunrise < .now && solar.safeSunset > .now
+		
+		if isDaytime {
+			return .result(value: Date().distance(to: solar.safeSunset))
+		} else if solar.safeSunset < .now {
+			return .result(value: TimeInterval(0))
+		} else if solar.safeSunrise > .now {
+			return .result(value: solar.daylightDuration)
+		} else {
+			return .result(value: TimeInterval(0))
+		}
+	}
+}
