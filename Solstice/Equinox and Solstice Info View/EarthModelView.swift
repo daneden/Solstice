7a9c91f90f5f975da65359c78ef8372ddf8ffafa
Daniel Eden <dan.eden@me.com>
2024-02-07 22:09:20 +0000

    Update EarthModelView

--- Solstice/Equinox and Solstice Info View/EarthModelView.swift
+++ Solstice/Equinox and Solstice Info View/EarthModelView.swift
@@ -10,28 +10,13 @@ import RealityKit
 
 #if os(visionOS)
 struct EarthModelView: View {
-	@State var realityViewContent: RealityViewContent?
-	
 	var body: some View {
-		RealityView { content in
-			if let earth = try? await Entity(named: "EarthScene") {
-				content.add(earth)
-				
-				realityViewContent = content
-			}
-		}
-		.task(id: realityViewContent?.entities.count) {
-			guard let earthGroupRoot = realityViewContent?.entities.first,
-						let earthGroup = earthGroupRoot.scene?.findEntity(named: "Group") else {
-				return
-			}
-			
-			var transform = earthGroup.transform
-			transform.rotation *= simd_quatf(
-				angle: Float(Angle(degrees: 180).radians),
-				axis: SIMD3<Float>(0, 1, 0)
-			)
-			earthGroup.move(to: transform, relativeTo: nil, duration: 4, timingFunction: .linear)
+		Model3D(named: "Earth") { model in
+			model
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+		} placeholder: {
+			ProgressView()
 		}
 	}
 }
