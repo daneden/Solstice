97aa3d09101a8951235cfc1811cea372de89b184
Daniel Eden <dan.eden@me.com>
2024-02-07 20:46:42 +0000

    Stub out visionOS earth model view

--- Solstice/Equinox and Solstice Info View/EarthModelView.swift
+++ Solstice/Equinox and Solstice Info View/EarthModelView.swift
@@ -6,12 +6,42 @@
 //
 
 import SwiftUI
+import RealityKit
 
+#if os(visionOS)
 struct EarthModelView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@State var realityViewContent: RealityViewContent?
+	
+	var body: some View {
+		RealityView { content in
+			if let earth = try? await Entity(named: "EarthScene") {
+				content.add(earth)
+				
+				realityViewContent = content
+			}
+		}
+		.task(id: realityViewContent?.entities.count) {
+			guard let earthGroupRoot = realityViewContent?.entities.first,
+						let earthGroup = earthGroupRoot.scene?.findEntity(named: "Group") else {
+				return
+			}
+			
+			var transform = earthGroup.transform
+			transform.rotation *= simd_quatf(
+				angle: Float(Angle(degrees: 180).radians),
+				axis: SIMD3<Float>(0, 1, 0)
+			)
+			earthGroup.move(to: transform, relativeTo: nil, duration: 4, timingFunction: .linear)
+		}
+	}
 }
+#else
+struct EarthModelView: View {
+	var body: some View {
+		EmptyView()
+	}
+}
+#endif
 
 #Preview {
     EarthModelView()
