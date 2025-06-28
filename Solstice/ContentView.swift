5b292fb3171f7ec3236f0264c08a65bd82df7870
Daniel Eden <dan.eden@me.com>
2025-06-28 23:20:17 +0100

    Fix build error

--- Solstice/ContentView.swift
+++ Solstice/ContentView.swift
@@ -138,7 +138,7 @@ struct ContentView: View {
 		
 		#if os(iOS)
 		ToolbarItem(placement: .bottomBar) {
-			Text("").accessibilityHidden().opacity(0)
+			Text("").accessibilityHidden(true).opacity(0)
 		}
 		#endif
 		
